da531c649f7c4971a85aec5da3b46fab43a8742c
Markel <markel.aguirre@opendeusto.es>
2024-05-28 10:40:08 +0200

    Correcciones en algunos campos de formularios y redireccion de clientes desde pedidos

--- proyecto1/empresaDjango/forms.py
+++ proyecto1/empresaDjango/forms.py
@@ -7,12 +7,11 @@ from .models import Categoria
 class PedidoForm(forms.ModelForm):
     class Meta:
         model = Pedido
-        fields = ['cliente', 'cod_pedido', 'fecha', 'estado']
+        fields = ['cliente', 'cod_pedido', 'fecha']
         widgets = {
             'cliente': forms.Select(attrs={'class': 'inputs-pedido'}),
             'cod_pedido': forms.TextInput(attrs={'class': 'inputs-pedido'}),
             'fecha': forms.DateInput(format="%d-%m-%Y", attrs={"type": "date", "class": "inputs-pedido"}),
-            'estado': forms.Select(attrs={'class': 'inputs-pedido'}),
         }
 
 #formulario de productos
